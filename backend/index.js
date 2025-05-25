5adcb3ead58f2c755de7852898380c9a9a8ee9f8
gabba <91146693+GabbaX2@users.noreply.github.com>
2025-05-25 23:01:26 +0200

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ const jwt = require('jsonwebtoken');
 const cors = require('cors');
 
 const corsOptions = {
-  origin: ['http://localhost:5173', 'https://forked-eight.vercel.app'],
+  origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
