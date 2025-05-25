df373ac1344ba333f4f9a174088737197a5aaf0b
gabba <91146693+GabbaX2@users.noreply.github.com>
2025-05-25 22:52:24 +0200

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -6,8 +6,14 @@ const jwt = require('jsonwebtoken');
 const cors = require('cors');
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
+
+app.options('*', cors()); 
 
 const auth = require('./middlewares/auth');
 const errorHandler = require('./middlewares/errorHandler');
