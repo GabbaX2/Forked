99c021813a898fffb2ea74e5ca2af328659f6e1d
gabba <91146693+GabbaX2@users.noreply.github.com>
2025-05-25 16:39:48 +0200

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -7,9 +7,6 @@ const cors = require('cors');
 
 const app = express();
 app.use(cors({
-  origin: 'https://forked-eight.vercel.app',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization']
 }));
 app.use(express.json());
 
